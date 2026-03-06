9a3973fdf2b3019f8affbaa7ce7318fa485d32eb
yoanny <manuelvaal2021@gmail.com>
2026-03-06 15:52:05 +0100

    add vercel site under cors for test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "https://client-project-dashboard-six.vercel.app",
     credentials: true,
   }))
 
