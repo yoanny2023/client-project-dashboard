4a3f96107e185d5eaa87238a5670662a397bbc59
yoanny <manuelvaal2021@gmail.com>
2026-03-08 14:16:51 +0100

    fix navbar fixed layout

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ function DashboardLayout({children}:{children: React.ReactNode}){
   const [isOpen, setIsOpen] = React.useState(false);
   return (
     <Pagina>
-       <div className="min-h-screen flex relative bg-[radial-gradient(circle_at_top,rgba(39,39,42,0.8),#09090b)]">
+       <div className="min-h-screen flex relative overflow-x-hidden bg-[radial-gradient(circle_at_top,rgba(39,39,42,0.8),#09090b)]">
       {/* Mobile Overlay */}
       {isOpen && (
         <div
@@ -19,16 +19,19 @@ function DashboardLayout({children}:{children: React.ReactNode}){
       )}
 
      <div
-        className={`fixed lg:static z-40 inset-y-0 left-0 w-64
+        className={`fixed z-40 inset-y-0 left-0 w-64
         transform ${isOpen ? "translate-x-0" : "-translate-x-full"}
         lg:translate-x-0 transition duration-300 ease-in-out`}
       >
         <Sidebar onClose={() => setIsOpen(false)} />
       </div>
 
-      <div className="flex-1 flex flex-col w-full">
-        <Navbar onMenuClick={() => setIsOpen(true)} />
-        <main className="flex-1 px-6 sm:px-13 py-6">{children}</main>
+      <div className="flex-1 flex flex-col w-full lg:ml-64">
+        <div className="fixed top-0 left-0 lg:left-64 right-0 z-30">
+          <Navbar onMenuClick={() => setIsOpen(true)} />
+        </div>
+
+        <main className="flex-1 px-6 sm:px-13 py-6 pt-20">{children}</main>
       </div>
     </div>
     </Pagina>  
