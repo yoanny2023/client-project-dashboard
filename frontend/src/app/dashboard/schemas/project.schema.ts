2dde9cb0e879bb21a4ee829a829a69f8cbb9f839
yoanny <manuelvaal2021@gmail.com>
2026-03-12 20:57:19 +0100

    fix trim name validation

--- frontend/src/app/dashboard/schemas/project.schema.ts
+++ frontend/src/app/dashboard/schemas/project.schema.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 export const projectSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(2, "At least 2 characters")
     .max(20, "Name is too long"),
 
   description: z
     .string()
+    .trim()
     .max(100, "Description too long")
     .optional(),
 });
